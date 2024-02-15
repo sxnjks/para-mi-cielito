0ae497e720ad4dc59badcff9f178f09e3354c478
sxnjks <160188179+sxnjks@users.noreply.github.com>
2024-02-15 20:25:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const yesBtn = document.querySelector(`#yesBtn`);
+
+yesBtn.addEventListener(`click`,function () {
+alert(`yo tambièn te amo mucho mi corazoncito lindo, feliz dia y recuerda que te amo mucho mucho, te iba a papear si decias q no bobo ñññññññññññ`)
+});
+
+const noBtn = document.querySelector(`#noBtn`);
+
+noBtn.addEventListener(`mouserover`,function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty(`top`,randomY+`%`);
+    noBtn.style.setProperty(`left`,randomX+`%`);
+    noBtn.style.setProperty(`transform`,`translate(-${randomX}%,-${randomY}%)`);
+}) 
+
+
+
